9d1dfaade1f0c7afc804537f935d462960cfdc77
Abhishek Gautam <abhishekgautm243@gmail.com>
2020-09-30 09:46:55 +0530

    Create menu.js

---
+++ src/pages/menu.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const MenuPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi from Menu</h1>
+  </Layout>
+)
+
+export default MenuPage
