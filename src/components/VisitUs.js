b3fe6678299cb5d6b504c9b3e277e79e607ee9a7
Abhishek Gautam <abhishekgautm243@gmail.com>
2020-09-30 16:39:23 +0530

    Update VisitUs.js

--- src/components/VisitUs.js
+++ src/components/VisitUs.js
@@ -2,11 +2,11 @@ import React from "react"
 
 const VisitUs = () => {
   return (
-    <div className="bg-orange-900 w-5/6 h-56 flex justify-around items-center">
-      <div className="text-white text-4xl tracking-wide font-light">
+    <div className="bg-orange-900 p-8 w-9/12 md:w-5/6 h-auto md:h-56 md:flex justify-around items-center">
+      <div className="text-white text-3xl pb-5 lg:text-4xl tracking-wide font-light">
         <h1>Visit Us</h1>
       </div>
-      <div className="text-gray-200 font-light text-lg">
+      <div className="text-gray-200 font-light text-sm md:text-base lg:text-lg tracking-wide">
         <h1 className="pb-3">236 N. Main St. Farmville, VA 23901</h1>
         <p>
           Open Monday - Friday 7:30am-3:00pm
