7c867dc1bf4093692a1ac16797d071191a4cae49
Abhishek Gautam <abhishekgautm243@gmail.com>
2020-09-30 09:46:43 +0530

    Create VisitUs.js

---
+++ src/components/VisitUs.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+const VisitUs = () => {
+  return (
+    <div className="bg-orange-900 w-5/6 h-56 flex justify-around items-center">
+      <div className="text-white text-4xl tracking-wide font-light">
+        <h1>Visit Us</h1>
+      </div>
+      <div className="text-gray-200 font-light text-lg">
+        <h1 className="pb-3">236 N. Main St. Farmville, VA 23901</h1>
+        <p>
+          Open Monday - Friday 7:30am-3:00pm
+          <br /> Saturday 8:00am-3:00pm
+          <br /> Closed on Sunday
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default VisitUs
