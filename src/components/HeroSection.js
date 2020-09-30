ea2648c0ce41b1d124418991d7d0d62596e432d7
Abhishek Gautam <abhishekgautm243@gmail.com>
2020-09-30 09:46:33 +0530

    Create HeroSection.js

---
+++ src/components/HeroSection.js
@@ -0,0 +1,29 @@
+import React from "react"
+import BackgroundImage from "gatsby-background-image"
+
+const HeroSection = ({ img, heroclass }) => {
+  return (
+    <BackgroundImage className={heroclass} fluid={img}>
+      <h1 className="text-white text-center md:text-6xl text-2xl px-2 mb-6 mt-32 lg:mb-8 lg:mt-12 tracking-wide leading-tight">
+        Organic Coffee & Teas,
+        <br />
+        100% Carytown Coffee Beans
+      </h1>
+      <div className="mb-16">
+        <h4 className="text-white text-center font-thin md:text-2xl text-xs px-20 tracking-wide">
+          Connecting with People through Coffee, Food, and Live Music.
+        </h4>
+      </div>
+      <div className="flex space-x-12">
+        <button className="bg-orange-300 py-2 px-4 text-xs md:text-sm md:py-3 md:px-6 hover:bg-black hover:text-white focus:outline-none transition-all duration-300 ease-linear">
+          View Our Menu
+        </button>
+        <button className="py-2 px-4 text-xs md:text-sm md:py-3 md:px-6 text-white border border-white hover:bg-black hover:border-transparent focus:outline-none transition-all duration-300 ease-linear">
+          More About Us
+        </button>
+      </div>
+    </BackgroundImage>
+  )
+}
+
+export default HeroSection
