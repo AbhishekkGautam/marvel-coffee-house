5e5eb4b4ff996d479041b41bc26dc6b962467803
Abhishek Gautam <abhishekgautm243@gmail.com>
2020-09-30 11:50:58 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        heading: "2rem",
+      },
+    },
   },
   variants: {},
   plugins: [],
