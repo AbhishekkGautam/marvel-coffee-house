f97bf743d21f2ce708debacd2c400d3fc71e8413
Abhishek Gautam <abhishekgautm243@gmail.com>
2020-09-30 09:46:50 +0530

    Create contact.js

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ContactPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi from Contact</h1>
+  </Layout>
+)
+
+export default ContactPage
