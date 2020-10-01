f02540bb58739575287c198d03b55c8c25f95419
Abhishek Gautam <abhishekgautm243@gmail.com>
2020-10-01 21:49:52 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ export const query = graphql`
     img: file(relativePath: { eq: "hero-image.jpg" }) {
       childImageSharp {
         fluid {
-          ...GatsbyImageSharpFluid_withWebP
+          ...GatsbyImageSharpFluid_withWebp
         }
       }
     }
