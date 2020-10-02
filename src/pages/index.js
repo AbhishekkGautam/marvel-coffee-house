585c13cb4218669792435caedd6e1d1d1a45a300
Abhishek Gautam <abhishekgautm243@gmail.com>
2020-10-02 05:43:59 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,27 +6,12 @@ import SEO from "../components/seo"
 import HeroSection from "../components/HeroSection"
 import VisitUs from "../components/VisitUs"
 
-const IndexPage = ({ data }) => (
+const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <HeroSection
-      img={data.img.childImageSharp.fluid}
-      heroclass="min-h-screen flex justify-center items-center flex-col"
-    />
+    <HeroSection heroclass="min-h-screen flex justify-center items-center flex-col" />
     <VisitUs />
   </Layout>
 )
-
-export const query = graphql`
-  {
-    img: file(relativePath: { eq: "hero-image.jpg" }) {
-      childImageSharp {
-        fluid {
-          ...GatsbyImageSharpFluid_withWebp
-        }
-      }
-    }
-  }
-`
 
 export default IndexPage
