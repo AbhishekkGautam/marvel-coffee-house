4ebefb19858002e3dd1e3a5d75ef88869ef4f3ae
Abhishek Gautam <abhishekgautm243@gmail.com>
2020-10-03 17:44:13 +0530

    Update VisitUs.js

--- src/components/VisitUs.js
+++ src/components/VisitUs.js
@@ -2,19 +2,21 @@ import React from "react"
 
 const VisitUs = () => {
   return (
-    <div className="bg-orange-900 p-8 w-9/12 md:w-5/6 h-auto md:h-56 md:flex justify-around items-center">
-      <div className="text-white text-3xl pb-5 lg:text-4xl tracking-wide font-light">
-        <h1>Visit Us</h1>
+    <section className="bg-orange-200">
+      <div className="bg-orange-900 p-8 w-4/5 md:w-5/6 h-auto md:h-56 md:flex justify-around items-center">
+        <div className="text-white text-3xl pb-5 lg:text-4xl tracking-wide font-light">
+          <h1>Visit Us</h1>
+        </div>
+        <div className="text-gray-200 font-light text-sm md:text-base lg:text-lg tracking-wide">
+          <h1 className="pb-3">236 N. Main St. Farmville, VA 23901</h1>
+          <p>
+            Open Monday - Friday 7:30am-3:00pm
+            <br /> Saturday 8:00am-3:00pm
+            <br /> Closed on Sunday
+          </p>
+        </div>
       </div>
-      <div className="text-gray-200 font-light text-sm md:text-base lg:text-lg tracking-wide">
-        <h1 className="pb-3">236 N. Main St. Farmville, VA 23901</h1>
-        <p>
-          Open Monday - Friday 7:30am-3:00pm
-          <br /> Saturday 8:00am-3:00pm
-          <br /> Closed on Sunday
-        </p>
-      </div>
-    </div>
+    </section>
   )
 }
 
