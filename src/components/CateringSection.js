099aff70cc09a398ff6c0cbdb2d0aadc8c968fae
Abhishek Gautam <abhishekgautm243@gmail.com>
2020-10-05 17:26:06 +0530

    Update CateringSection.js

--- src/components/CateringSection.js
+++ src/components/CateringSection.js
@@ -15,7 +15,7 @@ const CateringSection = () => {
     }
   `)
   return (
-    <section className="pt-10 pb-5 md:pb-10 bg-orange-200">
+    <section className="pt-10 pb-5 md:py-20 bg-orange-200">
       <div className="max-w-screen-lg mx-auto w-5/6">
         <h1 className="text-center text-4xl md:text-4xl lg:text-5xl font-poppins font-light pt-6 lg:pt-8 tracking-wide">
           Catering
@@ -25,7 +25,7 @@ const CateringSection = () => {
             <Img fluid={data.file.childImageSharp.fluid} />
           </div>
           <div className="mt-4 md:ml-10 lg:pl-24">
-            <p className="text-lg font-light md:text-base lg:text-xl pt-8 md:pt-0 text-center md:text-left leading-relaxed">
+            <p className="text-lg font-light md:text-base lg:text-xl pt-8 md:pt-0  md:text-left leading-relaxed">
               We offer various catering options, from sandwich platters to boxed
               lunches. To discuss catering options and place an order,
               <br />
