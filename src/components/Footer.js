ca4da4bd652b0eb1ee3e4ce87e096169b137bfea
Abhishek Gautam <abhishekgautm243@gmail.com>
2020-10-09 11:07:44 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,37 @@
 import React from "react"
-
+import logo from "../images/logo.png"
+import { Link } from "gatsby"
+import { FaFacebook, FaTwitter } from "react-icons/fa"
 const Footer = () => {
   return (
     <footer>
-      <div className="w-full bg-gray-800 h-auto">
-        <div className="container mx-auto">
-          <h1 className="text-white text-center py-5">
-            Awesome copyright footer &copy; 2020
-          </h1>
+      <div className="w-full bg-orange-300 h-auto">
+        <div className="container mx-auto flex justify-around py-10 xl:py-16 items-center">
+          <div className="">
+            <Link to="/">
+              <img
+                src={logo}
+                alt="logo"
+                width="80px"
+                height="80px"
+                className="xl:w-32 xl:h-24"
+              />
+            </Link>
+          </div>
+          <div className="">
+            <h1 className="text-xs pb-1 xl:pb-3 xl:text-lg">Follow Us</h1>
+            <div className="flex justify-between">
+              <Link to="#" className="text-black" aria-label="Cart Icon">
+                <FaFacebook className="text-xl xl:text-2xl" />
+              </Link>
+              <Link to="#" className="text-black" aria-label="Cart Icon">
+                <FaTwitter className="text-xl xl:text-2xl" />
+              </Link>
+            </div>
+          </div>
+          <div className="">
+            <h1 className="text-xs xl:text-lg">Designed by Abhishek Gautam.</h1>
+          </div>
         </div>
       </div>
     </footer>
