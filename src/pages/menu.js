bd5581a92cda9d0505d383553e8004ad935efd77
Abhishek Gautam <abhishekgautm243@gmail.com>
2020-10-09 21:56:15 +0530

    Update menu.js

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,14 +1,42 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import Menu from "../components/Menu";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+const MenuPage = ({ data }) => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div className="h-56 md:h-80 mt-24 text-4xl md:text-5xl lg:text-6xl font-semibold text-white tracking-wide bg-gradient-to-r from-orange-500 to-orange-900 flex justify-center items-center">
+        <h1 className="font-poppins">Our Menu</h1>
+      </div>
+      <Menu items={data.menu} />
+    </Layout>
+  );
+};
 
-const MenuPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi from Menu</h1>
-  </Layout>
-)
+export const query = graphql`
+  {
+    menu: allContentfulCoffeeItem {
+      edges {
+        node {
+          id
+          title
+          description {
+            description
+          }
+          price
+          category
+          image {
+            fixed(width: 120, height: 120) {
+              ...GatsbyContentfulFixed_withWebp
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
-export default MenuPage
+export default MenuPage;
