c36f46ebe04b29283cfd944fe1441a5a8a604922
Abhishek Gautam <abhishekgautm243@gmail.com>
2020-10-10 17:14:22 +0530

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,20 +1,62 @@
 import React, { Component } from "react";
 import Img from "gatsby-image";
 
+const getCategories = items => {
+  let tempItems = items.map(items => {
+    return items.node.category;
+  });
+  let tempCategories = new Set(tempItems);
+  let categories = Array.from(tempCategories);
+  categories = ["all", ...categories];
+  return categories;
+};
+
 export default class Menu extends Component {
   constructor(props) {
     super(props);
     this.state = {
       items: props.items.edges,
       coffeeItems: props.items.edges,
+      categories: getCategories(props.items.edges),
     };
   }
+
+  handleItems = category => {
+    let tempItems = [...this.state.items];
+    if (category === "all") {
+      this.setState(() => {
+        return { coffeeItems: tempItems };
+      });
+    } else {
+      let items = tempItems.filter(({ node }) => node.category === category);
+      this.setState(() => {
+        return { coffeeItems: items };
+      });
+    }
+  };
+
   render() {
     if (this.state.items.length > 0) {
       return (
-        <section className="mt-10 mb-10">
+        <section className="mt-8 md:mt-10 mb-10">
           <div className="max-w-screen-lg mx-auto w-5/6 md:w-3/4">
             {/*Categories*/}
+            <div className="flex items-start justify-between flex-wrap mb-12 md:mb-16">
+              {this.state.categories.map((category, index) => {
+                return (
+                  <button
+                    type="button"
+                    key={index}
+                    className="py-2 px-8 mt-5 text-xs lg:text-lg capitalize rounded lg:py-2 lg:px-10 hover:text-white hover:bg-orange-700 border border-orange-700 focus:outline-none transition-all duration-300 ease-linear"
+                    onClick={() => {
+                      this.handleItems(category);
+                    }}
+                  >
+                    {category}
+                  </button>
+                );
+              })}
+            </div>
             {/*Items*/}
             <div className="grid grid-cols-1 lg:grid-cols-2 col-gap-10 gap-y-8 xl:col-gap-16">
               {this.state.coffeeItems.map(({ node }) => {
@@ -25,10 +67,10 @@ export default class Menu extends Component {
                     </div>
                     <div className="pl-5">
                       <div className="flex justify-between">
-                        <h2 className="text-lg font-medium md:text-xl xl:text-2xl">
+                        <h2 className="text-base capitalize font-medium md:text-xl xl:text-2xl">
                           {node.title}
                         </h2>
-                        <p className="bg-orange-200 text-lg md:text-xl xl:text-2xl px-2 rounded-md">
+                        <p className="bg-orange-200 text-base md:text-xl xl:text-2xl px-2 rounded-md">
                           ${node.price}
                         </p>
                       </div>
