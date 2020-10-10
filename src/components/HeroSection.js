1d0e650d28a1a7e3539d60f84818db42f327eac3
Abhishek Gautam <abhishekgautm243@gmail.com>
2020-10-10 17:33:53 +0530

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,6 +1,6 @@
-import React from "react"
-import BackgroundImage from "gatsby-background-image"
-
+import React from "react";
+import BackgroundImage from "gatsby-background-image";
+import { Link } from "gatsby";
 const HeroSection = ({ img, heroclass }) => {
   return (
     <BackgroundImage className={heroclass} fluid={img} fadeIn="soft">
@@ -15,21 +15,25 @@ const HeroSection = ({ img, heroclass }) => {
         </h1>
       </div>
       <div className="flex space-x-10 md:space-x-12">
-        <button
-          className="bg-orange-300 py-3 px-5 text-sm lg:py-3 lg:px-6 hover:bg-black hover:text-white focus:outline-none transition-all duration-300 ease-linear"
-          aria-label="View Our Menu"
-        >
-          View Our Menu
-        </button>
-        <button
-          className="py-2 px-4 text-sm lg:py-3 lg:px-6 text-white border border-white hover:bg-black hover:border-transparent focus:outline-none transition-all duration-300 ease-linear"
-          aria-label="More About Us"
-        >
-          More About Us
-        </button>
+        <Link to="/menu">
+          <button
+            className="bg-orange-300 py-3 px-5 text-sm lg:py-3 lg:px-6 hover:bg-black hover:text-white focus:outline-none transition-all duration-300 ease-linear"
+            aria-label="View Our Menu"
+          >
+            View Our Menu
+          </button>
+        </Link>
+        <Link to="/about">
+          <button
+            className="py-2 px-4 text-sm lg:py-3 lg:px-6 text-white border border-white hover:bg-black hover:border-transparent focus:outline-none transition-all duration-300 ease-linear"
+            aria-label="More About Us"
+          >
+            More About Us
+          </button>
+        </Link>
       </div>
     </BackgroundImage>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
