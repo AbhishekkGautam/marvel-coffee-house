96cbee5700795e30b9577f96f6af7112d113c887
Abhishek Gautam <abhishekgautm243@gmail.com>
2020-10-12 11:08:35 +0530

    Update VisitUs.js

--- src/components/VisitUs.js
+++ src/components/VisitUs.js
@@ -1,9 +1,9 @@
-import React from "react"
+import React from "react";
 
 const VisitUs = () => {
   return (
     <section className="bg-orange-200">
-      <div className="bg-orange-900 p-8 md:w-5/6 xl:w-9/12 h-auto md:h-56 xl:h-64 md:flex justify-around items-center">
+      <div className="bg-coffee p-8 md:w-5/6 xl:w-9/12 h-auto md:h-56 xl:h-64 md:flex justify-around items-center">
         <div className="text-white text-3xl pb-5 lg:text-4xl tracking-wide font-light">
           <h1>Visit Us</h1>
         </div>
@@ -17,7 +17,7 @@ const VisitUs = () => {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default VisitUs
+export default VisitUs;
