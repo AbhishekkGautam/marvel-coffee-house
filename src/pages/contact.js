8bb3adc66d9131360bbf5528b5f375844afa38a1
Abhishek Gautam <abhishekgautm243@gmail.com>
2020-10-12 11:08:38 +0530

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,14 +1,18 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import Contact from "../components/Contact";
 
 const ContactPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi from Contact</h1>
+    <SEO title="Contact" />
+    <div className="h-56 md:h-80 mt-24 text-4xl md:text-5xl lg:text-6xl font-semibold text-white tracking-wide bg-gradient-to-r from-orange-500 to-orange-900 flex justify-center items-center">
+      <h1 className="font-poppins">Drop A Message</h1>
+    </div>
+    <Contact />
   </Layout>
-)
+);
 
-export default ContactPage
+export default ContactPage;
