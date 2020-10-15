70f8aef9782f8d3e045c164df62e7664a9d2ec36
Abhishek Gautam <abhishekgautm243@gmail.com>
2020-10-15 13:32:24 +0530

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,18 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import About from "../components/About";
 
 const AboutPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi from About</h1>
+    <div className="h-56 md:h-80 mt-24 text-4xl md:text-5xl lg:text-6xl font-semibold text-white tracking-wide bg-gradient-to-r from-orange-500 to-orange-900 flex justify-center items-center">
+      <h1 className="font-poppins">About Us</h1>
+    </div>
+    <About />
   </Layout>
-)
+);
 
-export default AboutPage
+export default AboutPage;
