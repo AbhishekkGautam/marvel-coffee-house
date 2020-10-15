03c49484bbd03d1e391b4fddd37ded8c2609cd28
Abhishek Gautam <abhishekgautm243@gmail.com>
2020-10-15 13:32:21 +0530

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="mt-12 md:mt-16 mb-24">
+      <div className="max-w-screen-lg mx-auto w-5/6 md:w-3/4">
+        <p className="text-lg lg:text-2xl tracking-wide font-poppins">
+          Uptown Coffee Cafe in Victoria, Virginia opened in December of 2007.
+          We have thrived in our location in Farmville, Virginia since 2014. ​
+        </p>
+        <p className="text-lg lg:text-2xl tracking-wide font-poppins pt-5">
+          We are a family- owned business with a lot of passion for what we do!
+          At Uptown Coffee Cafe, we bring our community together through coffee,
+          food, and live music.
+        </p>
+        <p className="text-lg lg:text-2xl tracking-wide font-poppins pt-5 pb-6">
+          We proudly serve organic coffee and teas and offer plenty of menu
+          items to keep you coming back for more! ​ Gather with your friends
+          over delicious food, coffee, and ice cream and experience our heroic
+          hospitality!
+        </p>
+        <p className="text-lg lg:text-2xl tracking-wide font-poppins text-orange-700">
+          Drink local coffee!
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
